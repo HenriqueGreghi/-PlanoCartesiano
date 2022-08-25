15b998c51f36955155345aea45b82bf01c0e57da
Henrique Greghi <henriquevsg@gmail.com>
2022-08-25 11:03:52 -0300

    Primiro commit

---
+++ PlanoCartesiano/Cartesiano.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlanoCartesiano
+{
+    class Cartesiano
+    {
+        public int x { get; set; }
+        public int y { get; set; }
+        private string plano { get; set; }
+
+        public void Parametros()
+        {
+            if (x > 0 && y > 0)
+            {
+                plano = "Primeiro";
+            }
+            else if (x < 0 && y > 0)
+            {
+                plano = "Segundo";
+            }
+            else if (x < 0 && y < 0)
+            {
+                plano = "Terceiro";
+            }
+            else if (x > 0 && y < 0)
+            {
+                plano = "Quarto";
+            }
+            else
+            {
+
+            }
+
+        }
+
+        public void Imprimir()
+        {
+            Console.WriteLine(plano);
+        }
+    }
+}
---
+++ PlanoCartesiano/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace PlanoCartesiano
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cartesiano param = new Cartesiano();
+
+            Console.WriteLine("Entre com a medida do plano cartesiano :");
+            string[] valores = Console.ReadLine().Split(' ');
+            param.x = int.Parse(valores[0]);
+            param.y = int.Parse(valores[1]);
+
+            param.Parametros();
+            param.Imprimir();
+        }
+    }
+}
